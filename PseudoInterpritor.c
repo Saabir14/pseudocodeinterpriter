8626c6eb7f43f2f102cdcf5e1c339621f55b773d
Saabir14 <64490729+Saabir14@users.noreply.github.com>
2021-12-18 13:01:37 +0400

    a

--- PseudoInterpritor.c
+++ PseudoInterpritor.c
@@ -1,5 +1,8 @@
+#include <ctype.h>
+#include <stdbool.h>
 #include <stdio.h>
-#define LIMIT 1024
+#include <stdlib.h>
+#define LIMIT 2048
 
 int main(int argc, char *argv[])
 {
@@ -8,26 +11,24 @@ int main(int argc, char *argv[])
         printf("ERROR:\tIvalid Number Of Arguments\nExpected: 1\tHave: %i\n", argc - 1);
         return 1;
     }
-    if (argc == 2)
+    else if (argc == 2)
     {
         // Open argv[1]
         FILE *file = fopen(argv[1], "r");
-        if (!file)
+        if (file)
+        {
+            fgets()
+            printf("File;\n%s", line);
+        }
+        else
         {
             printf("ERROR:\tFile Named '%s' Dosent Exist", argv[1]);
             return -1;
         }
     }
-
-    char line[LIMIT];
-    if (argc == 1)
-    {
-        // Get Line
-        printf(">>> ");
-        fgets(line, LIMIT, stdin);
-    }
     else
     {
-        // Read Line From argv[1]
+        printf("Not Implemented Interpritor");
+        return 1;
     }
 }
